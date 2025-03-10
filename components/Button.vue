51a956d23176643f2eccef738d0f6bfad2a97a3d
Tyler Grinstead <tyler@tgdesign.io>
2025-03-10 18:49:39 -0400

    refresh

---
+++ components/Button.vue
@@ -0,0 +1,14 @@
+<script setup>
+	const props = defineProps({
+	what: String,
+	color: String
+	});
+</script>
+<template>
+	<div>
+		<div class="btn text-white border-none ">
+			{{ props.what }}
+			{{ props.color }}
+		</div>
+	</div>
+</template>
--- components/Hero.vue
+++ components/Hero.vue
@@ -1,12 +1,16 @@
 <script setup>
+let quote = "Get A Quote!"
 </script>
 
 <template>
     <div class="flex flex-col items-center justify-center relative max-w-[1920px] mx-auto">
-    <img src="./../assets/tree.png" class="opacity-[35%] ml-[30%] mr-[75%] top-[-70px]" alt="tree" />
-    <div class="flex flex-col top-48 fixed z-0">
-    <h1 class="shadow-white drop-shadow-lg text-4xl text-black font-light items-center mt-36 mx-auto">Breathe Life into Your Digital Presence: </h1>
-    <h1 class="shadow-white drop-shadow-lg text-3xl text-black font-thin items-center mx-auto">We Help Your Brand Thrive <span class="font-light text-green-600">Online.</span></h1>
+        <img src="./../assets/tree.png" class="opacity-[35%] ml-[30%] mr-[75%] max-h-[600px]" alt="tree" />
+        <div class="flex flex-col items-center fixed z-0">
+            <h1 class="shadow-white drop-shadow-lg text-4xl text-black font-light items-center mx-auto">Breathe Life
+                into Your Digital Presence: </h1>
+            <h1 class="shadow-white drop-shadow-lg text-3xl text-black font-thin items-center mx-auto">We Help Your
+                Brand Thrive <span class="font-medium text-green-600">Online.</span></h1>
+            <Button :what="quote" class="mt-10" />
+        </div>
     </div>
-    </div>
-  </template>
+</template>
