ebf2554067098e5078adda4a0662a8c9fe875a67
Tyler Grinstead <tyler@tgdesign.io>
2025-03-10 19:22:07 -0400

    hero and nav touches

--- components/Hero.vue
+++ components/Hero.vue
@@ -6,11 +6,11 @@ let quote = "Get A Quote!"
     <div class="flex flex-col items-center justify-center relative max-w-[1920px] mx-auto">
         <img src="./../assets/tree.png" class="opacity-[35%] ml-[30%] mr-[75%] max-h-[600px]" alt="tree" />
         <div class="flex flex-col items-center fixed z-0">
-            <h1 class="shadow-white drop-shadow-lg text-4xl text-black font-light items-center mx-auto">Breathe Life
+            <h1 class="shadow-white drop-shadow-lg md:text-4xl text-2xl text-black font-light items-center mx-auto text-center">Breathe Life
                 into Your Digital Presence: </h1>
-            <h1 class="shadow-white drop-shadow-lg text-3xl text-black font-thin items-center mx-auto">We Help Your
+            <h1 class="shadow-white drop-shadow-lg md:text-3xl text-lg text-black font-thin items-center mx-auto">We Help Your
                 Brand Thrive <span class="font-medium text-green-600">Online.</span></h1>
-            <Button :what="quote" class="mt-10" />
+            <Button :what="quote" class="mt-10 hover:btn-success" />
         </div>
     </div>
 </template>
--- components/navigation.vue
+++ components/navigation.vue
@@ -6,15 +6,18 @@
     <div class="ml-0 mr-auto flex row items-center">
       <NuxtLink to="/" class="pl-4 py-2 max-w-[160px]"><img alt="TGDesign.io logo" src="/assets/tgdio.webp"></NuxtLink>
     </div>
-    <div class="text-slate-950 text-lg flex justify-end p-2 ml-auto mr-2">
-      <NuxtLink to="/about/" class="text-slate-950 p-2 hover:underline cursor-pointer">About</NuxtLink>
-      <NuxtLink to="/work/" class="text-slate-950 p-2 hover:underline ">Services</NuxtLink>
-      <NuxtLink to="/pricing/" class="text-slate-950 p-2 hover:underline cursor-pointer">Pricing</NuxtLink>
-      <span class="p-2 hover:underline cursor-pointer">Contact</span>
+    <div class="text-slate-950 text-lg justify-end p-2 ml-auto mr-2 md:flex hidden">
+      <NuxtLink to="/about/" class="text-slate-950 p-2 hover:underline cursor-pointer hover:text-green-600">About</NuxtLink>
+      <NuxtLink to="/work/" class="text-slate-950 p-2 hover:underline hover:text-green-600">Services</NuxtLink>
+      <NuxtLink to="/pricing/" class="text-slate-950 p-2 hover:underline cursor-pointer hover:text-green-600">Pricing</NuxtLink>
+      <span class="p-2 hover:underline cursor-pointer hover:text-green-600">Contact</span>
       <div
           class="ml-1 my-0 mr-0 flex items-center justify-center bg-[#2bbf60] rounded-lg max-w-fit cursor-pointer hover:bg-green-600 transition-all duration-500">
-        <span class="px-1 m-2 text-[#fff] font-bold ">Get A Quote</span>
+        <span class="px-1 m-2 text-[#fff] font-bold">Get A Quote</span>
       </div>
     </div>
+     <button class="btn btn-square btn-ghost md:hidden block mr-4">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="h-5 w-5 stroke-current inline-block"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg>
+    </button>
   </div>
 </template>
