3dfa583ff26426e6043d133d13286c74ed848954
Tyler Grinstead <tyler@tgdesign.io>
2025-03-21 17:47:45 -0400

    fixed hero section alignment

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,24 +1,43 @@
 <script setup>
+import { onMounted, ref } from 'vue'
+
 const props = defineProps({
     quote: {
         type: String,
         default: "Get A Quote!"
     }
 })
+
+// Use dynamic import for the image
+const backgroundImageUrl = ref('')
+
+onMounted(() => {
+    // In Nuxt, we can use the /assets directory in the URL
+    backgroundImageUrl.value = '/assets/tree.png'
+})
 </script>
 
 <template>
-    <div class="flex flex-col items-center justify-center relative max-w-[1920px] mx-auto">
-        <img src="./../assets/tree.png" loading="lazy" width="600" height="600"
-            class="opacity-[35%] ml-[30%] mr-[75%] max-h-[600px]" alt="tree" />
-        <div
-            class="flex flex-col items-center absolute z-0 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+    <div class="relative py-20 sm:py-32 px-4 overflow-hidden max-w-[1920px] mx-auto min-h-[400px] sm:min-h-[500px]">
+        <!-- Background image with lowest z-index -->
+        <div class="absolute inset-0 z-0">
+            <img src="./../assets/tree.png" loading="lazy" alt="tree"
+                class="opacity-[0.35] absolute top-1/2 left-[35%] transform -translate-y-1/2 -translate-x-1/2 h-[500px] object-contain" />
+        </div>
+
+        <!-- Content with higher z-index -->
+        <div class="relative z-20 flex flex-col items-center w-full max-w-2xl mx-auto">
             <h1
-                class="shadow-white drop-shadow-lg md:text-4xl text-2xl text-black font-light items-center mx-auto text-center">
-                Breathe Life into Your Digital Presence: </h1>
-            <h1 class="shadow-white drop-shadow-lg md:text-3xl text-lg text-black font-thin items-center mx-auto">We
-                Help Your Brand Thrive <span class="font-medium text-green-600">Online.</span></h1>
-            <Button :what="props.quote" class="mt-10 hover:btn-success" />
+                class="shadow-white drop-shadow-lg text-2xl sm:text-3xl md:text-4xl text-black font-light mb-2 sm:mb-4 text-center">
+                Breathe Life into Your Digital Presence
+            </h1>
+            <h2
+                class="shadow-white drop-shadow-lg text-lg sm:text-xl md:text-2xl font-semibold text-black mb-8 sm:mb-10 text-center">
+                We Help Your Brand Thrive
+                <span class="font-medium text-green-600 inline sm:block mt-1">Online.</span>
+            </h2>
+
+            <Button :what="props.quote" class="mt-2 sm:mt-6 hover:btn-success" />
         </div>
     </div>
 </template>
--- components/navigation.vue
+++ components/navigation.vue
@@ -17,49 +17,54 @@ const closeMenu = () => {
 </script>
 
 <template>
-  <div class="relative">
-    <div class="mx-auto bg-slate-50 flex space-between items-center">
-      <div class="ml-0 mr-auto flex row items-center">
-        <NuxtLink to="/" class="pl-4 py-2 max-w-[160px]"><img loading="lazy" width="160" height="40"
-            alt="TGDesign.io logo" src="/assets/tgdio.webp"></NuxtLink>
-      </div>
-      <div class="text-slate-950 text-lg justify-end p-2 ml-auto mr-2 md:flex hidden">
-        <NuxtLink :key="'about'" to="/about/"
-          class="text-slate-950 p-2 hover:underline cursor-pointer hover:text-green-600">About</NuxtLink>
-        <NuxtLink :key="'work'" to="/work/" class="text-slate-950 p-2 hover:underline hover:text-green-600">Services
-        </NuxtLink>
-        <NuxtLink :key="'pricing'" to="/pricing/"
-          class="text-slate-950 p-2 hover:underline cursor-pointer hover:text-green-600">Pricing</NuxtLink>
-        <span class="p-2 hover:underline cursor-pointer hover:text-green-600">Contact</span>
-        <div
-          class="ml-1 my-0 mr-0 flex items-center justify-center bg-[#2bbf60] rounded-lg max-w-fit cursor-pointer hover:bg-green-600 transition-all duration-500">
-          <span class="px-1 m-2 text-[#fff] font-bold">Get A Quote</span>
+  <div class="relative bg-slate-50 w-full">
+    <div class="container mx-auto px-4">
+      <div class="max-w-5xl mx-auto flex justify-between items-center">
+        <div class="flex items-center">
+          <NuxtLink to="/" class="py-4"><img loading="lazy" width="160" height="40" alt="TGDesign.io logo"
+              src="/assets/tgdio.webp"></NuxtLink>
         </div>
+        <div class="text-slate-950 text-lg justify-end p-2 md:flex hidden">
+          <NuxtLink :key="'about'" to="/about/"
+            class="text-slate-950 p-2 hover:underline cursor-pointer hover:text-green-600">About</NuxtLink>
+          <NuxtLink :key="'work'" to="/work/" class="text-slate-950 p-2 hover:underline hover:text-green-600">Services
+          </NuxtLink>
+          <NuxtLink :key="'pricing'" to="/pricing/"
+            class="text-slate-950 p-2 hover:underline cursor-pointer hover:text-green-600">Pricing</NuxtLink>
+          <span class="p-2 hover:underline cursor-pointer hover:text-green-600">Contact</span>
+          <div
+            class="ml-1 flex items-center justify-center bg-[#2bbf60] rounded-lg max-w-fit cursor-pointer hover:bg-green-600 transition-all duration-500">
+            <span class="px-1 m-2 text-[#fff] font-bold">Get A Quote</span>
+          </div>
+        </div>
+        <button @click="toggleMenu" class="btn btn-square btn-ghost md:hidden block" aria-label="Menu">
+          <span v-html="isMenuOpen ? closeIcon : menuIcon"></span>
+        </button>
       </div>
-      <button @click="toggleMenu" class="btn btn-square btn-ghost md:hidden block mr-4" aria-label="Menu">
-        <span v-html="isMenuOpen ? closeIcon : menuIcon"></span>
-      </button>
     </div>
 
     <!-- Mobile menu dropdown -->
     <div v-if="isMenuOpen"
       class="absolute w-full bg-slate-50 shadow-lg z-50 md:hidden transition-all duration-300 ease-in-out"
       :class="{ 'opacity-100 translate-y-0': isMenuOpen, 'opacity-0 -translate-y-10': !isMenuOpen }">
-      <div class="flex flex-col py-2">
-        <NuxtLink @click="closeMenu" :key="'about-mobile'" to="/about/"
-          class="text-slate-950 p-3 border-b border-slate-200 hover:bg-slate-100 hover:text-green-600">About</NuxtLink>
-        <NuxtLink @click="closeMenu" :key="'work-mobile'" to="/work/"
-          class="text-slate-950 p-3 border-b border-slate-200 hover:bg-slate-100 hover:text-green-600">Services
-        </NuxtLink>
-        <NuxtLink @click="closeMenu" :key="'pricing-mobile'" to="/pricing/"
-          class="text-slate-950 p-3 border-b border-slate-200 hover:bg-slate-100 hover:text-green-600">Pricing
-        </NuxtLink>
-        <a @click="closeMenu" href="#contact"
-          class="text-slate-950 p-3 border-b border-slate-200 hover:bg-slate-100 hover:text-green-600">Contact</a>
-        <div class="p-3">
-          <div
-            class="bg-[#2bbf60] rounded-lg p-2 text-center cursor-pointer hover:bg-green-600 transition-all duration-500">
-            <span class="text-white font-bold">Get A Quote</span>
+      <div class="container mx-auto px-4">
+        <div class="max-w-5xl mx-auto flex flex-col py-2">
+          <NuxtLink @click="closeMenu" :key="'about-mobile'" to="/about/"
+            class="text-slate-950 p-3 border-b border-slate-200 hover:bg-slate-100 hover:text-green-600">About
+          </NuxtLink>
+          <NuxtLink @click="closeMenu" :key="'work-mobile'" to="/work/"
+            class="text-slate-950 p-3 border-b border-slate-200 hover:bg-slate-100 hover:text-green-600">Services
+          </NuxtLink>
+          <NuxtLink @click="closeMenu" :key="'pricing-mobile'" to="/pricing/"
+            class="text-slate-950 p-3 border-b border-slate-200 hover:bg-slate-100 hover:text-green-600">Pricing
+          </NuxtLink>
+          <a @click="closeMenu" href="#contact"
+            class="text-slate-950 p-3 border-b border-slate-200 hover:bg-slate-100 hover:text-green-600">Contact</a>
+          <div class="p-3">
+            <div
+              class="bg-[#2bbf60] rounded-lg p-2 text-center cursor-pointer hover:bg-green-600 transition-all duration-500">
+              <span class="text-white font-bold">Get A Quote</span>
+            </div>
           </div>
         </div>
       </div>
