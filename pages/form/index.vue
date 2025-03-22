ef6db04d8103a5180759b3e5b138ea920bbd83f1
Tyler Grinstead <tyler@tgdesign.io>
2025-03-22 11:50:38 -0400

    Refactor form structure in ContactForm component to enhance user experience. Updated form fields with labels, added a role selection dropdown, and changed the form name to 'contact'. Improved layout for better clarity and usability.

--- pages/form/index.vue
+++ pages/form/index.vue
@@ -1,11 +1,22 @@
 <template>
-    <div>
-        <h1>Test Form</h1>
-    </div>
-    <form name="test" method="POST" data-netlify="true">
-        <input type="text" name="name" />
-        <input type="email" name="email" />
-        <textarea name="message"></textarea>
-        <button type="submit">Submit</button>
-    </form>
+<form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
 </template>
